4b6039f81aead296fdd0960a5f2a86fcb4f7c882
chiarua <2721783259@qq.com>
2023-12-23 22:12:54 +0800

    wtf

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,17 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    if(!head||!head->next){
+        return 0;
+    }
+    node *hare = head->next->next;
+    node *turtle = head->next;
+    while(hare!=turtle){
+        if(hare == NULL||!hare->next){
+            return 0;
+        }
+        hare = hare->next->next;
+        turtle = turtle->next;
+    }
+    return 1;
 }
